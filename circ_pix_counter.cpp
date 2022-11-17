ac7dc9dbab6f2775881718c7b9797a2f2c359294
Lars Øvergård <lars.overgard@hotmail.com>
2022-11-17 15:56:43 +0100

    feat: Prints program- and file-name

--- circ_pix_counter.cpp
+++ circ_pix_counter.cpp
@@ -11,6 +11,10 @@ int main() {
   // Make image instance 
   std::string file_name = "data.txt";
 
+  std::cout << "\n"
+          << "Starting 'Image 2 circle' converter" << "\n"
+          << "File: " << file_name << "\n\n";
+
   Image batman(file_name);
   batman.print_image();
   batman.print_dims();
